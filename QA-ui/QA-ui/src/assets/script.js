f2c2b165f77bbae1b4decd17cc9b34865cdba834
Sandeepa <sandeepaathauda2002@gmail.com>
2023-01-09 11:17:26 +0530

    commit

--- QA-ui/QA-ui/src/assets/script.js
+++ QA-ui/QA-ui/src/assets/script.js
@@ -1,10 +1,89 @@
+$(function () {
+    $(".signIn").click(function () {
 
-    $(function () {
-        $("#signIn").click(function () {
+        let email = $(".email").val();
+        let password = $(".password").val();
+
+        if (email == "") {
             Swal.fire(
-                'The Internet?',
-                'That thing is still around?',
-                'question'
-              )
-        });
+                'Email Address Should Not Be Empty', '',
+                'warning'
+            )
+        }
+        else if (password == "") {
+            Swal.fire(
+                'Password Should Not Be Empty', '',
+                'warning'
+            )
+        }else
+        {
+            window.location.href = "http://localhost:4200/register";
+        }
     });
+});
+
+
+$(function () {
+    $(".signUp").click(function () {
+
+        let email = $(".email").val();
+        let username = $(".uname").val();
+        let password = $(".pass").val();
+        let conpassword = $(".conpass").val();
+
+        if (email == "") {
+            Swal.fire(
+                'Email Address Should Not Be Empty', '',
+                'warning'
+            )
+        }
+        else if (username == "") {
+            Swal.fire(
+                'Username Should Not Be Empty', '',
+                'warning'
+            )
+        }
+        else if (password == "") {
+            Swal.fire(
+                'Password Should Not Be Empty', '',
+                'warning'
+            )
+        }
+        else if (conpassword == "") {
+            Swal.fire(
+                'Confirm Password Should Not Be Empty', '',
+                'warning'
+            )
+        }
+        else if (password != conpassword) {
+            Swal.fire(
+                'Confirm Password Not Matched', '',
+                'warning'
+            )
+        }else
+        {
+            window.location.href = "http://localhost:4200/login";
+        }
+    });
+});
+
+
+/*(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()*/
