9d9cc52cb7bfc7a1d7d743d08c3924f2ad60de0f
Sandeepa <sandeepaathauda2002@gmail.com>
2023-01-30 10:03:18 +0530

    add test for Q List

---
+++ QA-api/test/workflow-test.js
@@ -0,0 +1,39 @@
+process.env.NODE_ENV = 'test';
+const Question = require("../models/questions.model");
+const User = require("../models/newuser.model");
+const Answer = require("../models/answers.model");
+const chai = require("chai");
+const expect = chai.expect;
+const should = chai.should();
+const chaiHttp = require("chai-http");
+const server = require("../app");
+
+chai.use(chaiHttp);
+
+before((done) => {
+    //Question.deleteMany({}, function (err) {});
+    //User.deleteMany({}, function (err) {});
+    //Answer.deleteMany({}, function (err) {});
+    done();
+});
+
+after((done) => {
+    //Question.deleteMany({}, function (err) {});
+    //User.deleteMany({}, function (err) {});
+    //Answer.deleteMany({}, function (err) {});
+    done();
+});
+
+describe("GET Q List", () => {
+
+    it('Test Question List, are there any Questions', (done) => {
+        chai.request(server).get("/questions/list").end((err, res) => {
+            res.should.have.status(200);
+
+            expect(res.body.recordCount).to.be.above(0);
+            res.body.results.length.should.be.eql(res.body.recordCount);
+            done();
+        });
+    });
+});
+
