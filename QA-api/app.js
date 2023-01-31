641eb4bdc70bdbc4b7de310de3530c79057c075c
Sandeepa <sandeepaathauda2002@gmail.com>
2023-01-31 23:19:40 +0530

    test

--- QA-api/app.js
+++ QA-api/app.js
@@ -51,10 +51,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, function () {
-  console.log("Server is running on port:  " + PORT);
-});
-
-
 module.exports = app;
