4e738b0aa3bba995e52a0b235b2e1c1234b50a0f
Sandeepa <sandeepaathauda2002@gmail.com>
2023-01-31 17:02:39 +0530

    added test for models

--- QA-api/test/workflow-test.js
+++ QA-api/test/workflow-test.js
@@ -7,6 +7,7 @@ const expect = chai.expect;
 const should = chai.should();
 const chaiHttp = require("chai-http");
 const server = require("../app");
+const assert = require('assert');
 
 chai.use(chaiHttp);
 
@@ -23,6 +24,42 @@ after((done) => {
     //Answer.deleteMany({}, function (err) {});
     done();
 });
+
+var date = new Date();
+let pdate = date.toISOString().slice(0, 10);
+
+describe('Test Question Model', function () {
+    it('create and save question successfully', ()=> {
+
+      const question = new Question({ username: 'Tom', question: 'What is Unit testing?', date:pdate});
+  
+      return question.save().then(function (question) {
+        assert(question.username=='Tom');
+      });
+    });
+  });
+
+  describe('Test Answer Model', function () {
+    it('create and save Answer successfully', ()=> {
+
+      const answer = new Answer({ username: 'Sani', answers:'Unit Testing is a type of software testing where individual units or components of a software are tested.', date:pdate, pquestion: 'What is Unit testing?'});
+  
+      return answer.save().then(function (answer) {
+        assert(answer.username=='Sani');
+      });
+    });
+  });
+
+  describe('Test User Model', function () {
+    it('create and save User successfully', ()=> {
+
+      const user = new User({ email:"sani@gmail.com", username:"sani", password:"sani12345", title:"Software Developer", aboutme:"Hello Wolrd!", date:pdate});
+  
+      return user.save().then(function (user) {
+        assert(user.username=='sani');
+      });
+    });
+  });
 
 describe("GET Q List", () => {
 
@@ -176,7 +213,7 @@ describe("Update Answer", () => {
 
 /*----------------------------------------------------Test-User------------------------------------------------------------------------*/
 
-/*describe("POST User", () => {
+describe("POST User", () => {
     // After Testing Add User Please Comment this Test
     it('Test Add User', (done) => {
 
@@ -190,7 +227,7 @@ describe("Update Answer", () => {
             done();
         });
     });
-});*/
+});
 
 describe("Login User", () => {
 
