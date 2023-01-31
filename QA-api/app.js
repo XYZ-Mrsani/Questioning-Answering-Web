dcce7cf65070ac13151c6cd691e050dbe0d39d28
Sandeepa <sandeepaathauda2002@gmail.com>
2023-01-31 20:00:57 +0530

    change port

--- QA-api/app.js
+++ QA-api/app.js
@@ -51,10 +51,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, function () {
-  console.log("Server is running on port:  " + PORT);
-});
-
-
 module.exports = app;
